64a2fa78245502fc80438a672ad1be9653e18334
jithjob <jithjob@expressbase.com>
2017-01-27 11:24:25 +0530

    minor alignment fix

--- Objects/EbTextBox.cs
+++ Objects/EbTextBox.cs
@@ -42,18 +42,6 @@ namespace ExpressBase.Objects
             return ((!this.Hidden) ? this.UniqueString + this.RequiredString : string.Empty) + this.TextTransformString;
         }
 
-        public override string GetHtml()
-        {
-            return string.Format(@"
-<div style='position:absolute; left:{1}px; top:{2}px; {8}'>
-<div>{5}</div>
-<input type='{7}' name='{10}{0}' id='{0}' {6} style='width:{3}px; height:{4}px;  display:inline-block;' {9}/>
-<div style='display: inline-block;'></div>
-</div>",
-this.Name, this.Left, this.Top, this.Width, this.Height, 
-this.Label, this.MaxLengthString, this.TextModeString, this.HiddenString, (this.Required && !this.Hidden ? "required" : string.Empty),this.SkipPersistString);
-        }
-
         private string RequiredString
         {
             get { return (base.Required ? "$('#{0}').focusout(function() { isRequired(this); });".Replace("{0}", this.Name) : string.Empty); }
@@ -66,11 +54,7 @@ this.Label, this.MaxLengthString, this.TextModeString, this.HiddenString, (this.
 
         private string TextTransformString
         {
-            get {
-                return (((int)this.TextTransform > 0) ? "$('#{0}').keydown(function(event) { textTransform(this, {1}); }); $('#{0}').on('paste', function(event) { textTransform(this, {1}); });"
-                    .Replace("{0}", this.Name)
-                    .Replace("{1}", ((int)this.TextTransform).ToString()) : string.Empty);
-            }
+            get { return (((int)this.TextTransform > 0) ? "$('#{0}').keydown(function(event) { textTransform(this, {1}); }); $('#{0}').on('paste', function(event) { textTransform(this, {1}); });"  .Replace("{0}", this.Name) .Replace("{1}", ((int)this.TextTransform).ToString()) : string.Empty); }
         }
 
         private string UniqueString
@@ -82,35 +66,27 @@ this.Label, this.MaxLengthString, this.TextModeString, this.HiddenString, (this.
         {  
             get { return (this.MaxLength > 0) ? string.Format("maxlength='{0}'", this.MaxLength) : string.Empty; }
         }
-        private string SkipPersistString
-        {
-            get { return (base.SkipPersist ? "@Skip$" : string.Empty); }
-        }
 
         private string TextModeString
         {  
-            get {
+            get { string returnval = string.Empty; switch (this.TextMode) { case TextMode.Email: returnval = "email"; break; case TextMode.Password: returnval = "password"; break; case TextMode.Color: returnval = "color"; break; case TextMode.SingleLine: returnval = "text"; break; } return returnval; }
+        }
 
-                string returnval = string.Empty;
+        private string ReadOnlyString
+        {
+            get { return (base.ReadOnly ? "readonly" : string.Empty); }
+        }
 
-                switch (this.TextMode)
-                {
-                    case TextMode.Email:
-                        returnval = "email";
-                        break;
-                    case TextMode.Password:
-                        returnval = "password";
-                        break;
-                    case TextMode.Color:
-                        returnval = "color";
-                        break;
-                    case TextMode.SingleLine:
-                        returnval = "text";
-                        break;
-                }
-
-                return returnval;
-            }
+        public override string GetHtml()
+        {
+            return string.Format(@"
+<div style='position:absolute; left:{1}px; top:{2}px; {8}'>
+<div>{5}</div>
+<input type='{7}' name='{0}' id='{0}' {6} style='width:{3}px; height:{4}px;  display:inline-block;' {9} {10} />
+<div style='display: inline-block;'></div>
+</div>",
+this.Name, this.Left, this.Top, this.Width, this.Height,
+this.Label, this.MaxLengthString, this.TextModeString, this.HiddenString, (this.Required && !this.Hidden ? "required" : string.Empty), this.ReadOnlyString);
         }
     }
 }
