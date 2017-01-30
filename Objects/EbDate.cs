65985e7940f0223c55824441d8428cb48bc49f71
sureba73 <sureba@expressbase.com>
2017-01-30 18:54:51 +0530

    Date and Numeric controls Added

---
+++ Objects/EbDate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressBase.Objects
+{
+    [ProtoBuf.ProtoContract]
+    public class EbDate : EbControl
+    {
+        public EbDate() { }
+    }
+}
