1aee4c60b68114f5b6ebacc48e37898a764ecb2e
jithjob <jithjob@expressbase.com>
2017-02-07 12:10:36 +0530

    eb_calander,eb_txtbox

--- Objects/EbNumeric.cs
+++ Objects/EbNumeric.cs
@@ -14,25 +14,50 @@ namespace ExpressBase.Objects
         [System.ComponentModel.Category("Behavior")]
         public int MaxLength { get; set; }
 
+        [ProtoBuf.ProtoMember(2)]
+        [System.ComponentModel.Category("Behavior")]
+        public int DecimalPlaces { get; set; }
+
+        [ProtoBuf.ProtoMember(3)]
+        [System.ComponentModel.Category("Appearance")]
+        public decimal Value { get; set; }
+
+        [ProtoBuf.ProtoMember(4)]
+        private string PlaceHolder { get; set; }
+
         private string MaxLengthString
         {
-            get { return (this.MaxLength > 0) ? string.Format("maxlength='{0}'", this.MaxLength) : string.Empty; }
+            get { return ((this.MaxLength > 0) ? "$('#{0}').focus(function() {$(this).select();});".Replace("{0}", this.Name).Replace("{1}", this.Value.ToString()) : string.Empty); }
+        }
+
+        public override string GetHead()
+        {
+            return (this.MaxLengthString + @"
+$('.money').mask('#,##0.00', {
+    reverse: true,
+    translation: {
+    placeholder: '000,000.00'
+  }
+    }); ");
         }
 
         public override string GetHtml()
         {
             return string.Format(@"
 <div style='position:absolute; left:{1}px; top:{2}px; {7}'>
-<div style='{15} {16}'>{5}</div>
-<div  class='tooltp'><input type='number'  name='{0}' id='{0}' {6} style='width:{3}px; height:{4}px; {13} {14} display:inline-block;{9} {8} {12} />
-<div style='display: inline-block;'></div> {10}</div>
-<div class='helpText'> {11} </div>
+    <div style='{15} {16}'>{5}</div>
+    <div class='attachedlabel'>$</div>
+    <div  class='tooltp'>
+        <input type='text' step='0.1' name='{0}' value={17} placeholder={18} class='money' id='{0}' {6} style='width:{3}px; height:{4}px; {13} {14} display:inline-block;{9} {8} {12} />
+        <div style='display: inline-block;'></div> {10}
+    </div>
+    <div class='helpText'> {11} </div>
 </div>",
 this.Name, this.Left, this.Top, this.Width, this.Height, this.Label, this.MaxLengthString,//6
 this.HiddenString, (this.Required && !this.Hidden ? " required" : string.Empty), this.ReadOnlyString,//9
 ((this.ToolTipText == null) ? string.Empty : ((this.ToolTipText.Trim().Length == 0) ? string.Empty : ("<span class='tooltptext'>" + this.ToolTipText + "</span>"))),
 this.HelpText, "tabindex='" + this.TabIndex + "'",//12
- "background-color:" + this.BackColorSerialized + ";", "color:" + this.ForeColorSerialized + ";", "background-color:" + this.LabelBackColorSerialized + ";", "color:" + this.LabelForeColorSerialized + ";");
+ "background-color:" + this.BackColorSerialized + ";", "color:" + this.ForeColorSerialized + ";",  "background-color:" + this.LabelBackColorSerialized + ";", "color:" + this.LabelForeColorSerialized + ";", this.Value, (this.PlaceHolder.Trim() != ""|| this.PlaceHolder==null) ?  this.PlaceHolder  :"0.00");
         }
     }
 }
