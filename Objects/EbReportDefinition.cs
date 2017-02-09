6af8fdcedbca6c82d1ab3249776b94c3817037df
sureba73 <sureba@expressbase.com>
2017-02-09 20:10:11 +0530

    Report def

--- Objects/EbReportDefinition.cs
+++ Objects/EbReportDefinition.cs
@@ -9,6 +9,15 @@ namespace ExpressBase.Objects
     public class EbReportDefinition
     {
         [ProtoBuf.ProtoMember(1)]
+        public EbReportPaperSize PaperSize { get; set; }
+
+        [ProtoBuf.ProtoMember(2)]
+        public EbReportMargins Margins { get; set; }
+
+        [ProtoBuf.ProtoMember(3)]
+        public bool IsLandscape { get; set; }
+
+        [ProtoBuf.ProtoMember(1)]
         public EbControl ReportHeader { get; set; }
 
         [ProtoBuf.ProtoMember(2)]
@@ -22,5 +31,54 @@ namespace ExpressBase.Objects
 
         [ProtoBuf.ProtoMember(5)]
         public EbControl ReportFooter { get; set; }
+    }
+
+    [ProtoBuf.ProtoContract]
+    public class EbReportPaperSize
+    {
+        [ProtoBuf.ProtoMember(1)]
+        public string Name { get; set; }
+
+        [ProtoBuf.ProtoMember(2)]
+        public float Width { get; set; }
+
+        [ProtoBuf.ProtoMember(3)]
+        public float Height { get; set; }
+
+        public EbReportPaperSize(string name, float w, float h)
+        {
+            this.Name = name;
+            this.Width = w;
+            this.Height = h;
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+
+    [ProtoBuf.ProtoContract]
+    public class EbReportMargins
+    {
+        [ProtoBuf.ProtoMember(1)]
+        public float Left { get; set; }
+
+        [ProtoBuf.ProtoMember(2)]
+        public float Right { get; set; }
+
+        [ProtoBuf.ProtoMember(3)]
+        public float Top { get; set; }
+
+        [ProtoBuf.ProtoMember(4)]
+        public float Bottom { get; set; }
+
+        public EbReportMargins(float l, float r, float t, float b)
+        {
+            this.Left = l;
+            this.Right = r;
+            this.Top = t;
+            this.Bottom = b;
+        }
     }
 }
