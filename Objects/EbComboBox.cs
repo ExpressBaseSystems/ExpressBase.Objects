b3e20fcfe80e87dd5cfa3d8deec9ef69a89954a5
jithjob <jithjob@expressbase.com>
2017-03-09 13:09:22 +0530

    EbCombo

--- Objects/EbComboBox.cs
+++ Objects/EbComboBox.cs
@@ -5,6 +5,14 @@ using System.Threading.Tasks;
 
 namespace ExpressBase.Objects
 {
+    public enum DefaultSearchFor
+    {
+        BeginingWithKeyword,
+        EndingWithKeyword,
+        ExactMatch,
+        Contains,
+    }
+
     [ProtoBuf.ProtoContract]
     public class EbComboBox : EbControl
     {
@@ -45,6 +53,46 @@ namespace ExpressBase.Objects
         [System.ComponentModel.Category("Behavior")]
         public int MinLimit { get; set; }
 
+        [ProtoBuf.ProtoMember(11)]
+        [System.ComponentModel.Category("Behavior")]
+        public DefaultSearchFor DefaultSearchFor { get; set; }
+
+        [ProtoBuf.ProtoMember(12)]
+        [System.ComponentModel.Category("Behavior")]
+        public int NumberOfFields { get; set; }
+
+        private string VueDMcode
+        {
+            get
+            {
+                string rs = "";
+                for (int i = 1; i <= this.NumberOfFields; i++)
+                    rs += "displayMember$$:[],".Replace("$$", i.ToString());
+                return rs;
+            }
+        }
+
+        private string VueSelectcode
+        {
+            get
+            {
+                string rs = "<div id='{0}'>";
+                for (int i = 1; i <= this.NumberOfFields; i++)
+                    rs += @"
+<div style='display:inline-block;'>
+    <div  style='display:inline-block;' id='{0}Lbl'>label</div>
+    <v-select id='{0}$$' style='width:{3}px;' 
+        multiple
+	    v-model='displayMember$$'
+        :on-change='updateCk'
+        placeholder = 'Search...'>
+    </v-select>
+</div>".Replace("$$", i.ToString());
+                return rs+"</div>";
+            }
+        }
+
+
         public EbComboBox() { }
 
         public override string GetHead()
@@ -53,8 +101,11 @@ namespace ExpressBase.Objects
 $('#{0}_loading-image').hide();
 var VMindex;
 var DMindex;
+var DMembers={11};
+var DMindexes=[];
 var DtFlag = false;
 var cellTr=null;
+var Msearch_colName='';
 function InitDT(){
     $('#{0}_loading-image').show();
     $.get('/ds/columns/#######?format=json', function (data)
@@ -69,16 +120,15 @@ function InitDT(){
                     _c='dt-left';
                     if(value.columnName=='id')
                         _v = false;
-
                     if(value.columnName=='{4}')
                         VMindex = value.columnIndex;
+                    $.each(DMembers,function(j, v) {
+				        if(value.columnName== v){
+                            DMindexes.push(value.columnIndex);
+                        }
+			        });
                     if(value.columnName=='{5}')
                         DMindex = value.columnIndex;
-                    console.log('DMindex=' + DMindex);
-                    console.log('VMindex=' + VMindex);
-                    
-
-
                     if(value.columnIndex==0){                   
                         cols.push({'data':null, 'render': function ( data, type, row ) {return '<input type=\'checkbox\'>'}});                
                     }
@@ -131,6 +181,9 @@ function InitDT(){
 					                dq.searchtext=dq.searchtext+','+value;
 			                });              
 		                }
+
+                        if(Msearch_colName!=='')
+                            dq.Msearch_colName=Msearch_colName;
 	            },
                 dataSrc: function(dd) {
                                 $('#{0}_loading-image').hide();
@@ -145,23 +198,54 @@ function InitDT(){
        });
       
         //delayed search on combo searchbox
-            $( '#{0}container [type=search]').keyup($.debounce(500, function() {
-                    var search = $(this).val().toString();
-                    if(search.trim()!==''&& DtFlag){
-                        $('#{0}tbl').DataTable().search(search).draw();
-                        $('#{0}_loading-image').show();
+            $( '#{0}container [type=search]').keyup($.debounce(500, function(e) {
+                    if(isPrintable(e.which)){
+                        var search = $(this).val().toString();
+                        if(search.trim()!==''&& DtFlag){
+                            if( !search.startsWith('*') && !search.endsWith('*') ){
+                                if('{10}'==='BeginingWithKeyword'){
+                                    search = search +'%';
+                                }
+                                else if('{10}'==='EndingWithKeyword'){
+                                    search = '%'+search;
+                                }
+                                else if('{10}'==='ExactMatch'){
+                                    search = search;
+                                }
+                                else if('{10}'==='Contains'){
+                                    search = '%'+search+'%';
+                                }
+                            }
+                            else if(search.startsWith('*') && !search.endsWith('*')){
+                                search = '%'+search.slice(1);
+                            }
+                            else if(!search.startsWith('*') && search.endsWith('*')){
+                                search = search.slice(0, -1)+'%';
+                            }
+                            else if(search.startsWith('*') && search.endsWith('*')){
+                                search = '%'+search.slice(1, -1)+'%';
+                            }
+                            Msearch_colName=DMembers[e.target.id.replace('{0}srch','') - 1];
+                            $('#{0}tbl').DataTable().search(search).draw();
+                            $('#{0}_loading-image').show();
+                        }
                     }
             }));
 
         //double click  option in DD
             $('#{0}tbl tbody').on( 'dblclick', 'tr',function ( e ) {
+                var self = $(this);
 	            var Vmember=$('#{0}tbl').DataTable().row($(this)).data()[VMindex];
-                console.log('FRI' + data.columns.indexOf('tcredit') );
 	            var Dmember=$('#{0}tbl').DataTable().row($(this)).data()[DMindex];
                 if( !(Vobj{0}.valueMember.contains(Vmember)) ){
                     Vobj{0}.displayMember.push( Dmember );
                     Vobj{0}.valueMember.push( Vmember );
                     $(this).find('[type=checkbox]').prop('checked', true);
+    
+                    $.each(DMindexes,function(i,v){
+                            eval( 'Vobj{0}.displayMember'+ (i+1) +'.push( $(\'#{0}tbl\').DataTable().row(self).data()[v] );');
+                    });
+
                 }
             });
 
@@ -179,10 +263,19 @@ function InitDT(){
                if  (!(Vobj{0}.valueMember.contains(datas[VMindex])) ) {   
                     Vobj{0}.displayMember.push(datas[DMindex]);
                     Vobj{0}.valueMember.push(datas[VMindex]);
+
+                    $.each(DMindexes,function(i,v){
+                            eval( 'Vobj{0}.displayMember'+ (i+1) +'.push(datas[v]);');
+                    });
+
                 }
                else {
                     Vobj{0}.displayMember.splice(Vobj{0}.displayMember.indexOf(datas[DMindex]),1);
                     Vobj{0}.valueMember.splice(Vobj{0}.valueMember.indexOf(datas[VMindex]),1);
+
+                    $.each(DMindexes,function(i,v){
+                            eval( 'Vobj{0}.displayMember'+ (i+1) +'.splice(Vobj{0}.displayMember'+ (i+1) +'.indexOf(datas[v]),1);');
+                    });
                 }  
             });
 
@@ -201,7 +294,6 @@ function InitDT(){
                 var row = datatable.row( cell.index().row );
                 cellTr = row.nodes();
                 $( row.nodes() ).css('color', '#000');
-                $( row.nodes() ).animate({ 'zoom': 1.05 }, 10);
                 $(row.nodes()).css('font-weight', 'bold');	
                 $(row.nodes()).find('.focus').removeClass('focus');
                 $( row.nodes() ).addClass('selected');
@@ -210,7 +302,6 @@ function InitDT(){
             $('#{0}tbl').DataTable().on( 'key-blur', function ( e, datatable, cell ) {
                 var row = datatable.row( cell.index().row );
                 $(row.nodes()).css('color', '#333');
-                $( row.nodes()).animate({ 'zoom': 1 }, 10);
                 $(row.nodes()).css('font-weight', 'normal');
                 $( row.nodes() ).removeClass('selected');	
             });
@@ -224,6 +315,11 @@ function InitDT(){
                             Vobj{0}.displayMember.push( Dmember );
                             Vobj{0}.valueMember.push( Vmember );
                             $(cellTr).find('[type=checkbox]').prop('checked', true);
+
+                            $.each(DMindexes,function(i,v){
+                                eval( 'Vobj{0}.displayMember'+ (i+1) +'.push( $(\'#{0}tbl\').DataTable().row($(cellTr)).data()[v] );');
+                            });
+
                         }
                     }
              });
@@ -292,15 +388,14 @@ var Vobj{0} = new Vue({
                 data: {
                     options: [],
                     displayMember:[],
+                    {12}
                     valueMember:[],
                     id:'{0}',
                     DDstate:false
                 },
                 watch: {
                         valueMember: function (val) {
-                            console.log('watching' + val);
                             if( {6}===1 && !{8} && val.length >1){
-                                console.log('single case= ' + val);
                                 this.valueMember = this.valueMember.splice( 1, 1);
                                 this.displayMember = this.displayMember.splice( 1, 1);
                             }
@@ -312,12 +407,6 @@ var Vobj{0} = new Vue({
                         }
                 },
                 methods: {
-                    //searchOnGrid() {
-                    //    console.log('watching');
-                    //    if(this.valueMember.length>1){
-                    //        console.log(this.valueMember);
-                    //    }
-                    //},
                     toggleDD: function(){
                             this.DDstate=!this.DDstate;  
                             setTimeout(function(){
@@ -359,7 +448,9 @@ var Vobj{0} = new Vue({
 });
 
 //set id for searchBox
-    $( '#{0}container [type=search]').attr('id','{0}srch');
+    $( '#{0}container [type=search]').each( function (i) {
+        $(this).attr('id','{0}srch'+i);
+    });			
 
 //enter-DDenabling & if'' showall, esc arrow space key based DD enabling , backspace del-valueMember updating
     $( '#{0}container [type=search]').keydown(
@@ -374,7 +465,10 @@ var Vobj{0} = new Vue({
             }
             if( (e.which===8||e.which===46) &&search==='' && Vobj{0}.valueMember.length>0 ){
                 Vobj{0}.valueMember.pop();  
-                console.log('Vobj{0}.valueMember' +Vobj{0}.valueMember);
+                $.each(DMindexes,function(i,v){
+                        eval( 'Vobj{0}.displayMember'+ (i+1) +'.pop();');
+                });
+
             }
             if( e.which===40 ){
         	    Vobj{0}.showDD();
@@ -397,9 +491,12 @@ var Vobj{0} = new Vue({
     });
 
 //remove ids when tagclose button clicked
-    $( '.close').live('click', function(){
+     $( '#{0}container [class= close').live('click', function(){
                     Vobj{0}.valueMember.splice( delid(), 1);
-                    console.log(Vobj{0}.valueMember);
+                    $.each(DMindexes,function(i,v){
+                            eval( 'Vobj{0}.displayMember'+ (i+1) +'.splice( delid(), 1);');
+                    });
+
     });
 
 //hide DD when click outside select or DD &  required ( if  not reach minLimit) 
@@ -410,16 +507,15 @@ var Vobj{0} = new Vue({
         if ( (!container.is(e.target)&& container.has(e.target).length === 0) && (!container1.is(e.target)&& container1.has(e.target).length === 0) ){
                      Vobj{0}.hideDD();
                 if( Vobj{0}.valueMember.length<{7}&&{7}!==0 )
-                    document.getElementById('{0}srch').setCustomValidity('This field  require minimum {7} values');
+                    document.getElementById('{0}srch0').setCustomValidity('This field  require minimum {7} values');
                 else
                     if({9} && Vobj{0}.valueMember.length===0 )
-                        document.getElementById('{0}srch').setCustomValidity('This field  is required');
+                        document.getElementById('{0}srch0').setCustomValidity('This field  is required');
                     else
-                        document.getElementById('{0}srch').setCustomValidity('');
+                        document.getElementById('{0}srch0').setCustomValidity('');
 
         }
-    });
-  
+    });  
 "
 .Replace("{0}", this.Name)
 .Replace("#######", this.DataSourceId.ToString().Trim())
@@ -429,8 +525,12 @@ var Vobj{0} = new Vue({
 .Replace("{6}", ( !this.MultiSelect||this.MaxLimit==0 ) ? "1" : this.MaxLimit.ToString())
 .Replace("{7}", this.MinLimit.ToString())
 .Replace("{8}", this.MultiSelect.ToString().ToLower())
-.Replace("{9}", this.Required.ToString().ToLower());
-
+.Replace("{9}", this.Required.ToString().ToLower())
+.Replace("{10}", this.DefaultSearchFor.ToString())
+.Replace("{11}", "['acmaster1_name', 'tdebit', 'tcredit']")
+.Replace("{12}", this.VueDMcode);
+            
+           
         }
 
 
@@ -443,23 +543,22 @@ var Vobj{0} = new Vue({
     </script>
                
    <div id='{0}container' style='position:absolute; left:{1}px;  top:{2}px;'>
-    <v-select id='{0}' style='width:{3}px;' 
-        multiple
-	    v-model='displayMember'
-        :on-change='updateCk'
-        placeholder = 'Search...'>
-    </v-select>
-                <div id='{0}_loadingdiv' class='tbl-loadingdiv'>
-                    <img id='{0}_loading-image' width='25' src='/images/ajax-loader.gif' alt='Loading...' />
-               </div>
-    <div id='{0}DDdiv'v-show='DDstate' class='DDdiv'  style='width:{4}px;'> 
-        <table id='{0}tbl' tabindex='1000' class='display'></table>
+
+        {5}
+
+    <div id='{0}_loadingdiv' class='tbl-loadingdiv'>
+                <i id='{0}_loading-image' class='fa fa-spinner fa-pulse fa-2x fa-fw'></i>
+                <span class='sr-only'>Loading...</span>
     </div>
+    <center><div id='{0}DDdiv'v-show='DDstate' class='DDdiv'  style='width:{4}px;'> 
+        <table id='{0}tbl' tabindex='1000' class='display'></table>
+    </div></center>
 </div>"
+.Replace("{5}", this.VueSelectcode)
 .Replace("{0}", this.Name)
-.Replace("{1}", this.Left.ToString())
-.Replace("{2}", this.Top.ToString())
-.Replace("{3}", this.Width.ToString())
+.Replace("{1}", "150")//this.Left.ToString())
+.Replace("{2}", "200")//this.Top.ToString())
+.Replace("{3}", "200")//this.Width.ToString())
 .Replace("{4}", (this.DropdownWidth == 0) ? "300" : this.DropdownWidth.ToString());
         }
     }
