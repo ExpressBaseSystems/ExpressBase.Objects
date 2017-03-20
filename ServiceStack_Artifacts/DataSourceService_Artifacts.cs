361dfd86b2596c61cadfd29568d87e79f7488c6d
sureba73 <sureba@expressbase.com>
2017-03-20 12:47:45 +0530

    Moved SS Artifacts to Object for sharing convenience

---
+++ ServiceStack_Artifacts/DataSourceService_Artifacts.cs
@@ -0,0 +1,74 @@
+﻿using ExpressBase.Data;
+using ServiceStack;
+using ServiceStack.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace ExpressBase.Objects.ServiceStack_Artifacts
+{
+    [Route("/ds")]
+    [Route("/ds/data/{Id}")]
+    public class DataSourceDataRequest : IReturn<DataSourceDataResponse>
+    {
+        public int Id { get; set; }
+
+        public int Start { get; set; }
+
+        public int Length { get; set; }
+
+        public int Draw { get; set; }
+
+        public string SearchText { get; set; }
+
+        public string OrderByDirection { get; set; }
+
+        public string OrderColumnName { get; set; }
+
+        public string SearchColumnName { get; set; }
+
+        public string Token { get; set; }
+    }
+
+    [Route("/ds")]
+    [Route("/ds/columns/{Id}")]
+    public class DataSourceColumnsRequest : IReturn<DataSourceColumnsResponse>
+    {
+        public int Id { get; set; }
+
+        public string SearchText { get; set; }
+
+        public string OrderByDirection { get; set; }
+
+        public string SelectedColumnName { get; set; }
+
+        public string Token { get; set; }
+    }
+
+    [DataContract]
+    [Csv(CsvBehavior.FirstEnumerable)]
+    public class DataSourceDataResponse
+    {
+        [DataMember(Order = 1)]
+        public int Draw { get; set; }
+
+        [DataMember(Order = 2)]
+        public int RecordsTotal { get; set; }
+
+        [DataMember(Order = 3)]
+        public int RecordsFiltered { get; set; }
+
+        [DataMember(Order = 4)]
+        public RowColletion Data { get; set; }
+    }
+
+    [DataContract]
+    [Csv(CsvBehavior.FirstEnumerable)]
+    public class DataSourceColumnsResponse
+    {
+        [DataMember(Order = 1)]
+        public ColumnColletion Columns { get; set; }
+    }
+}
---
+++ ServiceStack_Artifacts/InfraService_Artifacts.cs
@@ -0,0 +1,89 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace ExpressBase.Objects.ServiceStack_Artifacts
+{
+    [DataContract]
+    public class InfraRequest : IReturn<InfraResponse>
+    {
+        [DataMember(Order = 0)]
+        public Dictionary<string, object> Colvalues { get; set; }
+
+        [DataMember(Order = 1)]
+        public string ltype { get; set; }
+    }
+
+    [DataContract]
+    public class InfraResponse
+    {
+        [DataMember(Order = 1)]
+        public int id { get; set; }
+    }
+
+    [DataContract]
+    [Route("/unc", "POST")]
+    public class UnRequest : IReturn<bool>
+    {
+        [DataMember(Order = 0)]
+        public Dictionary<string, object> Colvalues { get; set; }
+
+    }
+
+    [DataContract]
+    public class DbCheckRequest : IReturn<bool>
+    {
+        [DataMember(Order = 0)]
+        public Dictionary<string, object> DBColvalues { get; set; }
+
+        [DataMember(Order = 1)]
+        public int CId { get; set; }
+    }
+
+    [DataContract]
+    public class AccountRequest : IReturn<AccountResponse>
+    {
+        [DataMember(Order = 0)]
+        public Dictionary<string, object> Colvalues { get; set; }
+
+        [DataMember(Order = 1)]
+        public string op { get; set; }
+
+        [DataMember(Order = 2)]
+        public int TId { get; set; }
+    }
+
+    [DataContract]
+    public class AccountResponse
+    {
+        [DataMember(Order = 1)]
+        public int id { get; set; }
+    }
+
+    [DataContract]
+    public class GetAccount : IReturn<GetAccountResponse>
+    {
+        [DataMember(Order = 0)]
+        public int Uid { get; set; }
+
+        [DataMember(Order = 1)]
+        public string restype { get; set; }
+    }
+
+    [DataContract]
+    public class GetAccountResponse
+    {
+        [DataMember(Order = 1)]
+        public Dictionary<int, string> dict { get; set; }
+    }
+
+    [DataContract]
+    public class SendMail : IReturn<bool>
+    {
+        [DataMember(Order = 0)]
+        public Dictionary<string, object> Emailvals { get; set; }
+    }
+}
