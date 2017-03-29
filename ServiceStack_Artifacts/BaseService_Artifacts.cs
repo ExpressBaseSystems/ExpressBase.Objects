9a75eac2b91093f1ca8af83fb1df9372f9f79166
sureba73 <sureba@expressbase.com>
2017-03-29 13:13:25 +0530

    Request Base added for SS artifacts

---
+++ ServiceStack_Artifacts/BaseService_Artifacts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressBase.Objects.ServiceStack_Artifacts
+{
+    public interface IEbSSRequest
+    {
+        string Token { get; set; }
+
+        string TenantAccountId { get; set; }
+    }
+}
