88444d3e403a9fe74bc0a87415700b671a1f3733
RobyAbraham <robyabraham@expressbase.com>
2017-08-30 19:20:11 +0530

    dv object modified..

--- Objects/EbDataVisualization.cs
+++ Objects/EbDataVisualization.cs
@@ -27,6 +27,8 @@ namespace ExpressBase.Objects
         public string RenderAs { get; set; }
 
         public string IsPaged { get; set; }
+
+        public GOptions options { get; set; }
         public override void AfterRedisGet(RedisClient Redis)
         {
             try
@@ -57,59 +59,19 @@ namespace ExpressBase.Objects
         }
     }
 
-    //public class DTColumnDef
-    //{
-    //    public int data;
 
-    //    public string title;
+    public class GOptions
+    {
+        public List<axis> Xaxis { get; set; }
 
-    //    public string type;
+        public List<axis> Yaxis { get; set; }
 
-    //    public bool visible;
+        public string type { get; set; }
+    }
+    public class axis
+    {
+        public string index { get; set; }
 
-    //    public string name;
-
-    //    public string width;
-
-    //    public int pos;
-
-    //    public DTColumnDef(int data, string title, string type,bool vis, string name, string width, int pos)
-    //    {
-    //        this.data = data;
-    //        this.title = title;
-    //        this.type = type;
-    //        this.visible = vis;
-    //        this.name = name;
-    //        this.width = width;
-    //        this.pos = pos;
-    //    }
-    //}
-
-    //public class DTColumnExtPpty
-    //{
-    //    public string name;
-
-    //    public bool AggInfo;
-
-    //    public int DecimalPlace;
-
-    //    public string RenderAs;
-
-    //    public int pos;
-
-    //    public DTColumnExtPpty(string name, bool agginfo, int deci, string renderas, int pos)
-    //    {
-    //        this.name = name;
-    //        this.AggInfo = agginfo;
-    //        this.DecimalPlace = deci;
-    //        this.RenderAs = renderas;
-    //        this.pos = pos;
-    //    }
-
-    //    public DTColumnExtPpty(int pos)
-    //    {
-    //        this.pos = pos;
-    //    }
-
-    //}
+        public string name { get; set; }
+    }
 }
