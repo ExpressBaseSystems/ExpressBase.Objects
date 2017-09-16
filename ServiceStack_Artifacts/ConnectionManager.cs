90e2bc76bc43a82f1c5bc1a635ead7f9ee4051d2
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-09-16 20:45:54 +0530

    MongoDB and Conncetio Manager

---
+++ ServiceStack_Artifacts/ConnectionManager.cs
@@ -0,0 +1,13 @@
+﻿using ExpressBase.Objects.ServiceStack_Artifacts;
+
+namespace ExpressBase.Common.Connections
+{
+    public class GetConnectionsRequest : IEbSSRequest
+    {
+        public string TenantAccountId { get; set; }
+
+        public int UserId { get; set; }
+
+    }
+    
+}
