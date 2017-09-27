aebfeac87982a80a6a4c59102b9682fdf896ac62
Bini Varghese <binivarghese@expressbase.com>
2017-09-27 12:26:37 +0530

    new cs files for emailbuilder

---
+++ Objects/EmailRelated/EbEmailTemplatePG.cs
@@ -0,0 +1,22 @@
+﻿using ExpressBase.Common.Objects;
+using ExpressBase.Common.Objects.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Objects.Objects.EmailRelated
+{
+    public class EbEmailTemplatePG : EbEmailBuilder
+    {
+        [EnableInBuilder(BuilderType.EmailBuilder)]
+        new public string Name { get; set; }
+
+        [EnableInBuilder(BuilderType.EmailBuilder)]
+        public string Description { get; set; }
+
+        [EnableInBuilder(BuilderType.EmailBuilder)]
+        [PropertyEditor(PropertyEditorType.ObjectSelector)]
+        [OSE_ObjectTypes(EbObjectType.DataSource)]
+        public string DataSourceRefId { get; set; }
+    }
+}
