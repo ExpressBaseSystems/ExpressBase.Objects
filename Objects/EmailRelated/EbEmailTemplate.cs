0dd5cdbf1751035e08aff20b41abfb83abbe169e
Bini Varghese <binivarghese@expressbase.com>
2017-09-28 18:18:37 +0530

    added EbEmailTemplate.cs file

--- Objects/EmailRelated/EbEmailTemplate.cs
+++ Objects/EmailRelated/EbEmailTemplate.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ExpressBase.Objects.Objects
+namespace ExpressBase.Objects.EmailRelated
 {
     public enum EmailPriority
     {
@@ -14,18 +14,32 @@ namespace ExpressBase.Objects.Objects
         Low,
         Medium
     }
+
     [EnableInBuilder(BuilderType.EmailBuilder)]
-    public class EbEmailBuilder : EbObject
+    public class EbEmailTemplateBase : EbObject
     {
+
+    }
+
+    [EnableInBuilder(BuilderType.EmailBuilder)]
+    public class EbEmailTemplate : EbEmailTemplateBase
+    {
+        [EnableInBuilder(BuilderType.EmailBuilder)]
+        public string Description { get; set; }
+
         [EnableInBuilder(BuilderType.EmailBuilder)]
         public EmailPriority Priority { get; set; }
 
         [EnableInBuilder(BuilderType.EmailBuilder)]
-        [JsonConverter(typeof(Base64Converter))]
         public string Subject { get; set; }
 
         [EnableInBuilder(BuilderType.EmailBuilder)]
         [JsonConverter(typeof(Base64Converter))]
         public string Body { get; set; }
+
+        [EnableInBuilder(BuilderType.EmailBuilder)]
+        [PropertyEditor(PropertyEditorType.ObjectSelector)]
+        [OSE_ObjectTypes(EbObjectType.DataSource)]
+        public string DataSourceRefId { get; set; }
     }
 }
