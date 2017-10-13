354b0608f2d07d06fbe084f7d7a4145b4cbf2450
Bini Varghese <binivarghese@expressbase.com>
2017-10-13 10:58:47 +0530

    minor for unique check

--- ServiceStack_Artifacts/InfraServices_Artifacts2.cs
+++ ServiceStack_Artifacts/InfraServices_Artifacts2.cs
@@ -158,4 +158,25 @@ namespace ExpressBase.Objects.ServiceStack_Artifacts
         [DataMember(Order = 1)]
         public string TenantAccountId { get; set; }
     }
+    [DataContract]
+    public class UniqueRequest
+    {
+        [DataMember(Order = 1)]
+        public Dictionary<string, object> Colvalues { get; set; }
+
+    }
+
+    //[DataContract]
+    //public class GetUsersResponse : IEbSSResponse
+    //{
+
+    //    [DataMember(Order = 1)]
+    //    public Dictionary<string, object> Data { get; set; }
+
+    //    [DataMember(Order = 2)]
+    //    public string Token { get; set; }
+
+    //    [DataMember(Order = 3)]
+    //    public ResponseStatus ResponseStatus { get; set; }
+    //}
 }
