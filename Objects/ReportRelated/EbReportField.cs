72484f63137950ab5ab26a4d571cb433a62d7ca5
Amal K Dev <amaldev@expressbase.com>
2017-12-06 18:07:56 +0530

    summary field

--- Objects/ReportRelated/EbReportField.cs
+++ Objects/ReportRelated/EbReportField.cs
@@ -327,5 +327,33 @@ namespace ExpressBase.Objects.ReportRelated
 };";
         }
     }
+
+    [EnableInBuilder(BuilderType.Report)]
+    public class EbDataFieldSummary : EbReportField
+    {
+        [EnableInBuilder(BuilderType.Report)]
+        [HideInPropertyGrid]       
+        public string DataField { get; set; }
+
+        [EnableInBuilder(BuilderType.Report)]
+        public SummaryFunctions Function { get; set; }
+
+        public override string GetDesignHtml()
+        {
+            return "<div class='dropped' $type='@type' eb-type='DataFieldSummary' id='@id' style='border: @Border px solid;border-color: @BorderColor ; width: @Width px; background-color:@BackColor ; color:@ForeColor ; height: @Height px; position: absolute; left: @Left px; top: @Top px;'> @Title </div>".RemoveCR().DoubleQuoted();
+        }
+        public override string GetJsInitFunc()
+        {
+            return @"
+    this.Init = function(id)
+        {
+    this.Height =25;
+    this.Width= 200;
+    this.ForeColor = '#201c1c';
+    this.Border = 1;
+    this.BorderColor = '#aaaaaa'
+};";
+        }
+    }
 }
 
