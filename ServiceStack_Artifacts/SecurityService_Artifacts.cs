30d24e7bf197f75aac8ffade6d3392071306f023
Febin Carlos <33482855+febincarlos@users.noreply.github.com>
2018-01-16 19:08:58 +0530

    SecurityService_Artifacts  commonlist test

--- ServiceStack_Artifacts/SecurityService_Artifacts.cs
+++ ServiceStack_Artifacts/SecurityService_Artifacts.cs
@@ -9,6 +9,34 @@ using System.Text;
 
 namespace ExpressBase.Objects.ServiceStack_Artifacts
 {
+	//COMMON LIST-----------------------------------------
+	[DataContract]
+	public class GetUsersRequest1 : IReturn<GetUsersResponse1>, IEbSSRequest
+	{
+		[DataMember(Order = 1)]
+		public Dictionary<string, object> Colvalues { get; set; }
+		public string Token { get; set; }
+
+		public string TenantAccountId { get; set; }
+
+		public int UserId { get; set; }
+	}
+
+	[DataContract]
+	public class GetUsersResponse1 : IEbSSResponse
+	{
+
+		[DataMember(Order = 1)]
+		public Dictionary<string, object> Data { get; set; }
+
+		[DataMember(Order = 2)]
+		public string Token { get; set; }
+
+		[DataMember(Order = 3)]
+		public ResponseStatus ResponseStatus { get; set; }
+	}
+
+
 	//USER START-----------------------------------------
 	[DataContract]
 	public class GetManageUserRequest : IReturn<GetManageUserResponse>, IEbSSRequest
