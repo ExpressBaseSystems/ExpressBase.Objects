5873074ca06cb3597a07ea6d759f776b1e4413c5
ChinchuCVijayan <chinchucvijayan@expressbase.com>
2018-01-16 11:05:15 +0530

    test request response class

---
+++ ServiceStack_Artifacts/TestArtifacts.cs
@@ -0,0 +1,27 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ExpressBase.Objects.ServiceStack_Artifacts
+{
+    [DataContract]
+    public class TestRequest : EbServiceStackRequest, IReturn<EbDbCreateResponse>
+    {
+        //[DataMember(Order = 1)]
+        //public string dbName { get; set; }
+    }
+    [DataContract]
+    public class TestResponse : IEbSSResponse
+    {
+        [DataMember(Order = 1)]
+        public bool resp { get; set; }
+
+        [DataMember(Order = 2)]
+        public string Token { get; set; }
+
+        [DataMember(Order = 3)]
+        public ResponseStatus ResponseStatus { get; set; }
+    }
+}
