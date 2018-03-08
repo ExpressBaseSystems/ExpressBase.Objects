560c98f8117f044ed6db547771f608564dd817f2
DonajoseUllattil <donajose@expressbase.com>
2018-03-08 11:20:19 +0530

    Object for Font

---
+++ Helpers/FontStyle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Objects
+{
+    public class FontStyle
+    {
+        public FontStyle() { }
+
+        public string Font { get; set; }
+        public string Color { get; set; }
+        public string Case { get; set; }
+        public string Style { get; set; }
+        public int Size { get; set; }
+        public bool IsStrikeThrough { get; set; }
+        public bool IsUnderlined { get; set; }
+    }
+
+
+    //public enum FontStyles
+    //{
+    //    Regular,
+    //    Italic,
+    //    Bold,
+    //    ItalicBold
+    //}
+}
