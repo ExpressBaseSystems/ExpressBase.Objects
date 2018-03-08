93937d0174fdb985660c3a191cf741df8a6b0a53
DonajoseUllattil <donajose@expressbase.com>
2018-03-08 11:51:23 +0530

    EbFont class added

---
+++ Helpers/EbFont.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Objects
+{
+    public class EbFont
+    {
+        public EbFont() { }
+
+        public string Font { get; set; }
+        public int Size { get; set; }
+        public string Style { get; set; }
+        public FontWeights Weight { get; set; }
+        public string  color { get; set; }
+        public bool Caps { get; set; }
+        public bool Strikethrough { get; set; }
+        public bool Underline { get; set; }
+    }
+  //JS OBJ:  {"Font":"Abhaya Libre","Fontsize":"16","Fontstyle":"normal","FontWeight":"bold","Fontcolor":"#000000","Caps":"none","Strikethrough":"line-through","Underline":"none"}
+
+public enum FontWeights
+{
+    Regular,
+    Italic,
+    Bold,
+    ItalicBold
+}
+}
