bb0e33abacb094b3fa64d87be01c2bb896cbe6b4
Amal K Dev <amaldev@expressbase.com>
2018-05-10 20:07:03 +0530

    appdash ,devdash

--- ServiceStack_Artifacts/DevRelatedService_Artifacts.cs
+++ ServiceStack_Artifacts/DevRelatedService_Artifacts.cs
@@ -1,4 +1,5 @@
-﻿using ExpressBase.Common.EbServiceStack.ReqNRes;
+﻿using ExpressBase.Common;
+using ExpressBase.Common.EbServiceStack.ReqNRes;
 using ServiceStack;
 using System;
 using System.Collections.Generic;
@@ -92,7 +93,7 @@ namespace ExpressBase.Objects.ServiceStack_Artifacts
         public string TenantAccountId { get; set; }
 
         public int UserId { get; set; }
-    }
+    }  
 
     [DataContract]
     public class GetApplicationResponse : IEbSSResponse
@@ -107,8 +108,73 @@ namespace ExpressBase.Objects.ServiceStack_Artifacts
         public ResponseStatus ResponseStatus { get; set; }
     }
 
+    public class GetAllApplicationRequest : IReturn<GetAllApplicationResponse>, IEbSSRequest
+    {
+        [DataMember(Order = 1)]
+        public string Token { get; set; }
+
+        public string TenantAccountId { get; set; }
+
+        public int UserId { get; set; }
+    }
+
+    public class GetAllApplicationResponse : IEbSSResponse
+    {
+        [DataMember(Order = 1)]
+        public List<AppWrapper> Data { get; set; }
+
+        [DataMember(Order = 2)]
+        public string Token { get; set; }
+
+        [DataMember(Order = 3)]
+        public ResponseStatus ResponseStatus { get; set; }
+    }
+
+    [DataContract]
+    public class AppWrapper
+    {
+        [DataMember(Order = 1)]
+        public int Id { set; get; }
+
+        [DataMember(Order = 2)]
+        public string Name { set; get; }
+
+        [DataMember(Order = 3)]
+        public int AppType { set; get; }
+
+        [DataMember(Order = 4)]
+        public string Icon { set; get; }
+
+        [DataMember(Order = 5)]
+        public string Description { set; get; }
+
+    }
+
+    public class GetObjectsByAppIdRequest : IReturn<GetObjectsByAppIdResponse>, IEbSSRequest
+    {
+        [DataMember(Order = 1)]
+        public int Id { get; set; }
+
+        [DataMember(Order = 2)]
+        public EbApplicationTypes AppType { get; set; }
+
+        public string TenantAccountId { get; set; }
+
+        public int UserId { get; set; }
+    }
+
+    public class GetObjectsByAppIdResponse: IEbSSResponse
+    {
+        [DataMember(Order = 1)]
+        public Dictionary<int, TypeWrap> Data { get; set; }
+
+        [DataMember(Order = 2)]
+        public ResponseStatus ResponseStatus { get; set; }
+    }
+
     public class GetObjectRequest : EbServiceStackRequest, IReturn<GetObjectResponse>
     {
+
     }
 
     public class GetObjectResponse : IEbSSResponse
