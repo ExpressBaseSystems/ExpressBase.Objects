4f7f14654a02245d6b1afadd7e1a242bbf9cd96f
DonajoseUllattil <donajose@expressbase.com>
2018-08-02 11:23:27 +0530

    Implementation of DiscoverRelatedObjects - For Import Export Purpose

--- ObjectContainers/EbFilterDialog.cs
+++ ObjectContainers/EbFilterDialog.cs
@@ -3,8 +3,10 @@ using ExpressBase.Common.Extensions;
 using ExpressBase.Common.Objects;
 using ExpressBase.Common.Objects.Attributes;
 using ExpressBase.Common.Structures;
+using ServiceStack;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -34,10 +36,11 @@ namespace ExpressBase.Objects
                 else if ((c as EbDate).ShowDateAs_ == DateShowFormat.Year_Month_Date)
                     val = "01-01-2018";
                 Param _p = new Param { Name = c.Name, Type = Convert.ToInt32(c.EbDbType).ToString(), Value = val };
-                if (c is EbComboBox) {
-                   if ((c as EbComboBox).ValueMember.Type.ToString() == "Int32")
+                if (c is EbComboBox)
+                {
+                    if ((c as EbComboBox).ValueMember.Type.ToString() == "Int32")
                     {
-                        _p.Type ="11";
+                        _p.Type = "11";
                     }
                     //_p.Type = Convert.ToInt32((EbDbType)((c as EbComboBox).ValueMember.Type));
                 }
@@ -82,5 +85,12 @@ namespace ExpressBase.Objects
 
         [EnableInBuilder(BuilderType.FilterDialog)]
         public int Width { get; set; }
+
+        public override OrderedDictionary DiscoverRelatedObjects(IServiceClient ServiceClient, OrderedDictionary obj_dict)
+        {
+            if (!obj_dict.Contains(this.RefId))
+                obj_dict.Add(RefId, this);
+            return obj_dict;
+        }
     }
 }
--- Objects/EbDataSource.cs
+++ Objects/EbDataSource.cs
@@ -9,16 +9,15 @@ using ServiceStack;
 using ServiceStack.Redis;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ExpressBase.Objects
 {
-   
-    public class EbDatasourceMain : EbObject{
 
-    }
+    public class EbDatasourceMain : EbObject { }
 
     [EnableInBuilder(BuilderType.DataSource)]
     public class EbDataSource : EbDatasourceMain
@@ -62,7 +61,7 @@ namespace ExpressBase.Objects
             try
             {
                 this.FilterDialog = Redis.Get<EbFilterDialog>(this.FilterDialogRefId);
-                if (this.FilterDialog == null && this.FilterDialogRefId != "" )
+                if (this.FilterDialog == null && this.FilterDialogRefId != "")
                 {
                     var result = client.Get<EbObjectParticularVersionResponse>(new EbObjectParticularVersionRequest { RefId = this.FilterDialogRefId });
                     this.FilterDialog = EbSerializers.Json_Deserialize(result.Data[0].Json);
@@ -71,8 +70,31 @@ namespace ExpressBase.Objects
             }
             catch (Exception e)
             {
-                Console.WriteLine("Exception:"+ e.ToString());
+                Console.WriteLine("Exception:" + e.ToString());
             }
+        }
+        public override OrderedDictionary DiscoverRelatedObjects(IServiceClient ServiceClient, OrderedDictionary obj_dict)
+        {
+            if (!obj_dict.Contains(this.RefId))
+                obj_dict.Add(RefId, this);
+            var x = obj_dict[RefId];
+            if (!FilterDialogRefId.IsEmpty())
+            {
+                EbFilterDialog fd = FilterDialog;
+                if (fd is null)
+                {
+                    fd = GetObjfromDB(FilterDialogRefId, ServiceClient) as EbFilterDialog;
+                    fd.DiscoverRelatedObjects(ServiceClient, obj_dict);
+                }
+            }
+            return obj_dict;
+        }
+        public EbObject GetObjfromDB(string _refid, IServiceClient ServiceClient)
+        {
+            var res = ServiceClient.Get(new EbObjectParticularVersionRequest { RefId = _refid });
+            EbObject obj = EbSerializers.Json_Deserialize(res.Data[0].Json);
+            obj.RefId = _refid;
+            return obj;
         }
     }
 
