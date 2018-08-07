872e7589491cd91db4f0718e15840e7f89855b76
DonajoseUllattil <donajose@expressbase.com>
2018-08-07 17:38:41 +0530

    GetAllFromAppStoreRequest n Resp , AppStore class implemented

--- ServiceStack_Artifacts/ImportExportService_Artifacts.cs
+++ ServiceStack_Artifacts/ImportExportService_Artifacts.cs
@@ -9,15 +9,7 @@ namespace ExpressBase.Objects.ServiceStack_Artifacts
 {
     public class SaveToAppStoreRequest : EbServiceStackRequest, IReturn<SaveToAppStoreResponse>
     {
-        public string AppName { get; set; }
-
-        public int Status { get; set; }
-
-        public int Cost { get; set; }
-
-        public string Json { get; set; }
-
-        public string Currency { get; set; }
+        public AppStore Store { get; set; }
     }
 
     public class SaveToAppStoreResponse : IEbSSResponse
@@ -44,5 +36,39 @@ namespace ExpressBase.Objects.ServiceStack_Artifacts
 
         [DataMember(Order = 3)]
         public ResponseStatus ResponseStatus { get; set; }
+    }
+
+    public class GetAllFromAppStoreRequest : EbServiceStackRequest, IReturn<GetAllFromAppstoreResponse>
+    {
+    }
+    public class GetAllFromAppstoreResponse : IEbSSResponse
+    {
+        [DataMember(Order = 1)]
+        public List<AppStore> Apps { get; set; }
+
+        [DataMember(Order = 2)]
+        public string Token { get; set; }
+
+        [DataMember(Order = 3)]
+        public ResponseStatus ResponseStatus { get; set; }
+    }
+
+    public class AppStore
+    {
+        public int Id { get; set; }
+
+        public string AppName { get; set; }
+
+        public int Status { get; set; }
+
+        public int Cost { get; set; }
+
+        public string Json { get; set; }
+
+        public string Currency { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime CreatedAt { get; set; }
     }
 }
