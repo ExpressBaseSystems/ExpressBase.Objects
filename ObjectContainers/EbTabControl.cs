c1ab5c1cbdf78917c54d5e5606ceb5e3c1b7363a
Jith Job K.I <jithjob@expressbase.com>
2018-09-11 18:02:20 +0530

    tabrelated, adjustPanesHeightToHighest fn added

--- ObjectContainers/EbTabControl.cs
+++ ObjectContainers/EbTabControl.cs
@@ -26,6 +26,9 @@ namespace ExpressBase.Objects
                 padding : function(elementId, props) {
                     $(`#${ elementId}.Eb-ctrlContainer > .tab-content >.tab-pane`).css('padding', props.Padding + 'px');
                 }
+                adjustPanesHeightToHighest : function(elementId, props) {
+                    alert();
+                },
             }";
             }
         }
@@ -41,7 +44,9 @@ namespace ExpressBase.Objects
         [PropertyEditor(PropertyEditorType.Collection)]
         [Alias("TabCollection")]
         [PropertyGroup("test")]
+        [UIproperty]
         [ListType(typeof(EbTabPane))]
+        [OnChangeUIFunction("adjustPanesHeightToHighest")]
         public override List<EbControl> Controls { get; set; }
 
         public override string GetDesignHtml()
@@ -62,11 +67,11 @@ this.Init = function(id)
 
         public override string GetHtml()
         {
-            string TabBtnHtml = "<div id='cont_@name@' ebsid='@name@' class='Eb-ctrlContainer' Ctype='TabControl'><ul class='nav nav-tabs'>".Replace("@name@", Name);
+            string TabBtnHtml = "<div id='cont_@ebsid@' ebsid='@ebsid@' class='Eb-ctrlContainer' Ctype='TabControl'><ul class='nav nav-tabs'>".Replace("@ebsid@", EbSid);
             string TabContentHtml = "<div class='tab-content'>";
 
             foreach (EbControl tab in Controls)
-                TabBtnHtml += "<li li-of='@name@' @active><a data-toggle='tab' href='#@name@'>@name@</a></li>".Replace("@name@", tab.Name);
+                TabBtnHtml += "<li li-of='@ebsid@' @active><a data-toggle='tab' href='#@ebsid@'>@name@</a></li>".Replace("@name@", tab.Name).Replace("@ebsid@", tab.EbSid);
 
             TabBtnHtml += "</ul>";
 
@@ -101,7 +106,7 @@ this.Init = function(id)
 
         public override string GetHtml()
         {
-            string html = "<div id='@name@' ebsid='@ebsid@' class='tab-pane fade @inactive'>";
+            string html = "<div id='@ebsid@' ebsid='@ebsid@' class='tab-pane fade @inactive'>";
 
             foreach (EbControl ec in this.Controls)
                 html += ec.GetHtml();
