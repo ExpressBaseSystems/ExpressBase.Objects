0bc6500f04719e6ac43ebbc51fed0cd9d6f3fb7f
RobyAbraham <robyabraham@expressbase.com>
2018-10-09 22:21:04 +0530

    grouping text substring..

--- ServiceStack_Artifacts/DataSourceService_Artifacts.cs
+++ ServiceStack_Artifacts/DataSourceService_Artifacts.cs
@@ -512,9 +512,9 @@ namespace ExpressBase.Objects.ServiceStack_Artifacts
                 {
                     for (int itr = 0; itr < CurrentLevel; itr++)
                         tempstr += "<td> &nbsp;</td>";
+                    string cleanedHeaderText = (CurrentLevel == 1) ? GroupingTexts[CurrentLevel - 1].Substring(2, GroupingTexts[CurrentLevel - 1].Length - 2) : GroupingTexts[CurrentLevel - 1];
                     base.Html = string.Format("<tr class='group' group='{0}'>{1}<td><i class='fa fa-minus-square-o' style='cursor:pointer;'></i></td><td colspan='{2}'>{3} ({4})</td></tr>",
-                    base.CurrentLevel /*+ 1*/, tempstr, base.ColumnCount.ToString(),
-                    GroupingTexts[CurrentLevel - 1].Substring(2, GroupingTexts[CurrentLevel - 1].Length - 2), (CurrentLevel == TotalLevels) ? base.GroupingCount.ToString() : base.LevelCount.ToString());
+                    base.CurrentLevel /*+ 1*/, tempstr, base.ColumnCount.ToString(), cleanedHeaderText, (CurrentLevel == TotalLevels) ? base.GroupingCount.ToString() : base.LevelCount.ToString());
                 }
                 else
                 {
