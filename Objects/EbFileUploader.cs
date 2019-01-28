d012dafd350a443737f3b9457b627d3312be5614
Febin Carlos <febincarlos@expressbase.com>
2019-01-28 19:33:01 +0530

    FileUploader initializing, rendering

--- Objects/EbFileUploader.cs
+++ Objects/EbFileUploader.cs
@@ -2,6 +2,7 @@
 using ExpressBase.Common.Extensions;
 using ExpressBase.Common.Objects;
 using ExpressBase.Common.Objects.Attributes;
+using ExpressBase.Objects.Helpers;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -72,39 +73,34 @@ namespace ExpressBase.Objects
         public override string GetBareHtml()
         {
             return @" 
-        <div class='input-group' style='width:100%;'>
-            <input id='@name@txt' type='text' style='width:100%;' onclick=""$('#@name@').click()""  />
-            <input id='@name@' data-toggle='tooltip' title='@toolTipText@' type='file' onchange=""$('#@name@txt').val($(this).val())"" name='@name@' @value@ @tabIndex@ style='display:none; width:100%; @BackColor@ @ForeColor@ @fontStyle@ @readOnlyString@ @required@ />
-            <span class='input-group-addon' onclick=""$('#@name@').click()""> <i id='@name@TglBtn' class='fa  fa-file' aria-hidden='true'></i> </span>
-        </div>"
-.Replace("@name@", this.Name)
-.Replace("@toolTipText@", this.ToolTipText)
-.Replace("@value@", "")//"value='" + this.Value + "'")
-.Replace("@tabIndex@", "tabindex='" + this.TabIndex + "'")
-    .Replace("@BackColor@ ", ("background-color:" + ((this.BackColor != null) ? this.BackColor : "@BackColor@ ") + ";"))
-    .Replace("@ForeColor@ ", "color:" + ((this.ForeColor != null) ? this.ForeColor : "@ForeColor@ ") + ";")
-.Replace("@required@", " required")//(this.Required && !this.Hidden ? " required" : string.Empty))
-.Replace("@readOnlyString@", this.ReadOnlyString);
+        <div id='@ebsid@' style='width:100%;'></div>"
+            .Replace("@ebsid@", this.EbSid);
         }
 
         public override string GetHtml()
         {
-            string EbCtrlHTML = @"
-    <div id='cont_@name@' Ctype='FileUploader' class='Eb-ctrlContainer' style='@hiddenString'>
-        <div class='eb-ctrl-label' id='@name@Lbl' style='@LabelBackColor  @LabelForeColor '> @Label </div>
-       @barehtml@
-        <span class='helpText'> @HelpText </span>
-    </div>
-"
-.Replace("@barehtml@", this.GetBareHtml())
-.Replace("@name@", this.Name)
-.Replace("@hiddenString", this.HiddenString)
+            //            string EbCtrlHTML = @"
+            //    <div id='cont_@name@' Ctype='FileUploader' class='Eb-ctrlContainer' style='@hiddenString'>
+            //        <div class='eb-ctrl-label' id='@name@Lbl' style='@LabelBackColor  @LabelForeColor '> @Label </div>
+            //       @barehtml@
+            //        <span class='helpText'> @HelpText </span>
+            //    </div>
+            //"
+            //.Replace("@barehtml@", this.GetBareHtml())
+            //.Replace("@name@", this.Name)
+            //.Replace("@hiddenString", this.HiddenString)
 
-    .Replace("@LabelForeColor ", "color:" + ((this.LabelForeColor != null) ? this.LabelForeColor : "@LabelForeColor ") + ";")
-    .Replace("@LabelBackColor ", "background-color:" + ((this.LabelBackColor != null) ? this.LabelBackColor : "@LabelBackColor ") + ";")
-    .Replace("@HelpText ", ((this.HelpText != null) ? this.HelpText : "@HelpText "))
-    .Replace("@Label ", ((this.Label != null) ? this.Label : "@Label "));
-            return EbCtrlHTML;
+            //    .Replace("@LabelForeColor ", "color:" + ((this.LabelForeColor != null) ? this.LabelForeColor : "@LabelForeColor ") + ";")
+            //    .Replace("@LabelBackColor ", "background-color:" + ((this.LabelBackColor != null) ? this.LabelBackColor : "@LabelBackColor ") + ";")
+            //    .Replace("@HelpText ", ((this.HelpText != null) ? this.HelpText : "@HelpText "))
+            //    .Replace("@Label ", ((this.Label != null) ? this.Label : "@Label "));
+            //            return EbCtrlHTML;
+
+            string EbCtrlHTML = HtmlConstants.CONTROL_WRAPER_HTML4WEB
+               .Replace("@LabelForeColor ", "color:" + (LabelForeColor ?? "@LabelForeColor ") + ";")
+               .Replace("@LabelBackColor ", "background-color:" + (LabelBackColor ?? "@LabelBackColor ") + ";");
+
+            return ReplacePropsInHTML(EbCtrlHTML);
         }
     }
 }
