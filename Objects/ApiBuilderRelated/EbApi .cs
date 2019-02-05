95b2ae7dad0da7b46d763093ca7ea1c052c1a352
Amal K Dev <amaldev@expressbase.com>
2019-02-05 17:41:41 +0530

    api

--- Objects/ApiBuilderRelated/EbApi .cs
+++ Objects/ApiBuilderRelated/EbApi .cs
@@ -9,10 +9,12 @@ using System.Text;
 using ExpressBase.Common.Extensions;
 using Newtonsoft.Json;
 using ExpressBase.Common;
+using ExpressBase.Common.Data;
+using System.Linq;
 
 namespace ExpressBase.Objects
 {
-    public class ListOrdered: List<EbApiWrapper>
+    public class ListOrdered : List<EbApiWrapper>
     {
         public ListOrdered()
         {
@@ -34,10 +36,12 @@ namespace ExpressBase.Objects
         public virtual string Refid { get; set; }
 
         public object Result { set; get; }
+
+        public virtual object GetOutParams(List<Param> _param) { return null; }
     }
 
     [EnableInBuilder(BuilderType.ApiBuilder)]
-    public class EbApi: EbApiWrapper
+    public class EbApi : EbApiWrapper
     {
         public override int RouteIndex { set; get; }
 
@@ -59,6 +63,21 @@ namespace ExpressBase.Objects
         {
             return "<div class='apiPrcItem dropped' eb-type='SqlReader' id='@id'><div tabindex='1' class='drpbox' onclick='$(this).focus();'> @Label </div></div>".RemoveCR().DoubleQuoted(); ;
         }
+
+        public override object GetOutParams(List<Param> _param)
+        {
+            List<Param> p = new List<Param>();
+            foreach (EbDataTable table in (this.Result as EbDataSet).Tables)
+            {
+                string[] c = _param.Select(item => item.Name).ToArray();
+                foreach (EbDataColumn cl in table.Columns)
+                {
+                    if (c.Contains(cl.ColumnName))
+                        p.Add(new Param { Name = cl.ColumnName, Type = cl.Type.ToString(), Value = table.Rows[0][cl.ColumnIndex].ToString() });
+                }
+            }
+            return p;
+        }
     }
 
     [EnableInBuilder(BuilderType.ApiBuilder)]
@@ -74,6 +93,11 @@ namespace ExpressBase.Objects
         {
             return "<div class='apiPrcItem dropped' eb-type='SqlFunc' id='@id'><div tabindex='1' class='drpbox' onclick='$(this).focus();'> @Label </div></div>".RemoveCR().DoubleQuoted();
         }
+
+        public override object GetOutParams(List<Param> _param)
+        {
+            return null;
+        }
     }
 
     [EnableInBuilder(BuilderType.ApiBuilder)]
@@ -88,6 +112,32 @@ namespace ExpressBase.Objects
         public override string GetDesignHtml()
         {
             return "<div class='apiPrcItem dropped' eb-type='SqlWriter' id='@id'><div tabindex='1' class='drpbox' onclick='$(this).focus();'> @Label </div></div>".RemoveCR().DoubleQuoted();
+        }
+
+        public override object GetOutParams(List<Param> _param)
+        {
+            return null;
+        }
+    }
+
+    [EnableInBuilder(BuilderType.ApiBuilder)]
+    public class EbEmailNode : EbApiWrapper
+    {
+        [EnableInBuilder(BuilderType.ApiBuilder)]
+        [PropertyEditor(PropertyEditorType.ObjectSelector)]
+        [PropertyGroup("Data Settings")]
+        [OSE_ObjectTypes(EbObjectTypes.iEmailBuilder)]
+        public override string Refid { get; set; }
+
+        public override string GetDesignHtml()
+        {
+            return "<div class='apiPrcItem dropped' eb-type='EmailNode' id='@id'><div tabindex='1' class='drpbox' onclick='$(this).focus();'> @Label </div></div>".RemoveCR().DoubleQuoted();
+        }
+
+        public override object GetOutParams(List<Param> _param)
+        {
+            
+            return null;
         }
     }
 }
