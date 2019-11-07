7cf8a945bbfdbdf5c87dd67d344f43accd838815
Amal k dev <amaldev@expressbase.com>
2019-11-07 18:16:24 +0530

    mobile page

---
+++ Objects/MobilePage/EbMobilePage.cs
@@ -0,0 +1,20 @@
+﻿using ExpressBase.Common.Objects;
+using ExpressBase.Common.Objects.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Objects
+{
+    public abstract class EbMobilePageBase : EbObject
+    {
+
+    }
+
+    [EnableInBuilder(BuilderType.MobilePage)]
+    [BuilderTypeEnum(BuilderType.MobilePage)]
+    public class EbMobilePage : EbMobilePageBase,IEBRootObject
+    {
+
+    }
+}
