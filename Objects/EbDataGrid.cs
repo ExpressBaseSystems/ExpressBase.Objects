a80d62a9ba71fd82f3800d5b8465ffa670a8f9f9
VarghesePaulose <51479206+VarghesePaulose@users.noreply.github.com>
2019-11-22 16:03:31 +0530

    title expression

--- Objects/EbDataGrid.cs
+++ Objects/EbDataGrid.cs
@@ -196,8 +196,8 @@ $.each(this.Controls.$values, function (i, col) {
         {
             string html = @"
 <div class='grid-cont'>
-    <div class='addrow-btn'>+  Row</div>
-    <div class='Dg_head'>
+    <div class='addrow-btn' tabindex='0'>+  Row</div>
+	<div class='Dg_head'>
         <table id='tbl_@ebsid@_head' class='table table-bordered dgtbl'>
             <thead>
               <tr>
