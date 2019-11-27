789958e1dffa3f7ecea37e08463a5f61dc6a511b
Amal k dev <amaldev@expressbase.com>
2019-11-27 19:20:49 +0530

    mobilepage meta

---
+++ Objects/MobilePage/MobilePageMeta.cs
@@ -0,0 +1,41 @@
+﻿using ExpressBase.Common.Structures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Objects.Objects.MobilePage
+{
+    public class MobileFormData
+    {
+        public string MasterTable { set; get; }
+
+        public int LocationId { set; get; }
+
+        public List<MobileTable> Tables { set; get; }
+    }
+
+    public class MobileTable : List<MobileTableRow>
+    {
+        public string TableName { set; get; }
+    }
+
+    public class MobileTableRow
+    {
+        public string RowId { set; get; }
+
+        public bool IsUpdate { set; get; }
+
+        public List<MobileTableColumn> Columns { set; get; }
+    }
+
+    public class MobileTableColumn
+    {
+        public string Name { get; set; }
+
+        public object Value { get; set; }
+
+        public EbDbTypes Type { set; get; }
+
+        public EbMobileControl Control { set; get; }
+    }
+}
