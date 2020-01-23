ba7e8a724c6eebfbc44c7d4c415c8fc8b1e2402e
Febin Carlos <febincarlos@expressbase.com>
2020-01-23 18:30:12 +0530

    GetDynamicGridData fixes

--- ObjectContainers/WebFormRelated/QueryGetter.cs
+++ ObjectContainers/WebFormRelated/QueryGetter.cs
@@ -95,15 +95,13 @@ namespace ExpressBase.Objects.WebFormRelated
 
                 _qryCount++;
 
-                //foreach (ColumnSchema Col in _table.Columns)
-                //{
-                //    if (Col.Control.DoNotPersist)
-                //        continue;
-                //    if (Col.Control is EbPowerSelect)
-                //        _queryPs += (Col.Control as EbPowerSelect).GetSelectQuery(DataDB, _service, Col.ColumnName, _table.TableName, _id, _this.FormSchema.MasterTable);
-                //    else if (Col.Control is EbDGPowerSelectColumn)
-                //        _queryPs += (Col.Control as EbDGPowerSelectColumn).GetSelectQuery(DataDB, _service, Col.ColumnName, _table.TableName, _id, _this.FormSchema.MasterTable);
-                //}
+                foreach (ColumnSchema Col in _table.Columns)
+                {
+                    if (Col.Control.DoNotPersist)
+                        continue;
+                    if (Col.Control is EbDGPowerSelectColumn)
+                        _queryPs += (Col.Control as EbDGPowerSelectColumn).GetSelectQuery123(DataDB, _service, _table.TableName, Col.ColumnName, _prntTbl, _this.FormSchema.MasterTable);
+                }
             }
             return query;
         }
