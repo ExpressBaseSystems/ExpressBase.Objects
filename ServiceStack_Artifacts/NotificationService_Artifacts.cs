f3f5e0754f1e4b62c8db5fa82dc6072ca4174ec4
anoopa <anoopa.baby@expressbase.com>
2020-02-19 14:54:36 +0530

    Notification - Artifacts

---
+++ ServiceStack_Artifacts/NotificationService_Artifacts.cs
@@ -0,0 +1,92 @@
+﻿using ExpressBase.Common;
+using ExpressBase.Common.EbServiceStack.ReqNRes;
+using ExpressBase.Common.ServerEvents_Artifacts;
+using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ExpressBase.Objects.ServiceStack_Artifacts
+{
+    public class NotifyLogOutRequest : EbServiceStackAuthRequest, IReturn<NotifyLogOutResponse>
+    {
+    }
+
+    public class NotifyLogOutResponse : IEbSSResponse
+    {
+        [DataMember(Order = 1)]
+        public ResponseStatus ResponseStatus { get; set; }
+
+    }
+
+    public class NotifyByUserIDRequest : EbServiceStackAuthRequest, IReturn<NotifyByUserIDResponse>
+    {
+        public int UsersID { get; set; }
+    }
+
+    public class NotifyByUserIDResponse : IEbSSResponse
+    {
+        [DataMember(Order = 1)]
+        public ResponseStatus ResponseStatus { get; set; }
+
+    }
+
+    public class NotifyByUserRoleRequest : EbServiceStackAuthRequest, IReturn<NotifyByUserRoleResponse>
+    {
+        public string RoleName { get; set; }
+    }
+
+    public class NotifyByUserRoleResponse : IEbSSResponse
+    {
+        [DataMember(Order = 1)]
+        public ResponseStatus ResponseStatus { get; set; }
+
+    }
+
+    public class NotifyByUserGroupRequest : EbServiceStackAuthRequest, IReturn<NotifyByUserGroupResponse>
+    {
+        public string GroupName { get; set; }
+    }
+
+    public class NotifyByUserGroupResponse : IEbSSResponse
+    {
+        [DataMember(Order = 1)]
+        public ResponseStatus ResponseStatus { get; set; }
+
+    }
+
+    public class GetNotificationFromDbRequest : EbServiceStackAuthRequest, IReturn<GetNotificationFromDbResponse>
+    {
+        public string NotificationId { get; set; }
+    }
+
+    public class GetNotificationFromDbResponse : IEbSSResponse
+    {
+        [DataMember(Order = 1)]
+        public ResponseStatus ResponseStatus { get; set; }
+
+        [DataMember(Order = 2)]
+        public List<NotificationInfo> Notifications { get; set; }
+    }
+
+    public class Notifications 
+    {
+        public List<NotificationInfo> Notification { get; set; }
+    }
+    
+    public class GetNotificationsRequest : EbServiceStackAuthRequest, IReturn<GetNotificationsResponse>
+    {
+    }
+
+    public class GetNotificationsResponse : IEbSSResponse
+    {
+        [DataMember(Order = 1)]
+        public ResponseStatus ResponseStatus { get; set; }
+
+        [DataMember(Order = 2)]
+        public List<NotificationInfo> Notifications { get; set; }
+    }
+    
+}
