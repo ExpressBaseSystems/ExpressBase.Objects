938fc6e48be1a0fe42b7e2f8f20f5f5855058c46
VarghesePaulose <51479206+VarghesePaulose@users.noreply.github.com>
2020-02-26 10:29:01 +0530

    BP minor

--- Objects/EbBluePrint.cs
+++ Objects/EbBluePrint.cs
@@ -132,7 +132,7 @@ namespace ExpressBase.Objects
 			return @" 
  <div id='@ebsid@'  > 
         <div class='ebimg-cont' style='width:100%; text-align:center;'>
-            <img id='@name@' class='dpctrl-img' src='@src@'  style='opacity:0.5; max-width:100%; max-height:@maxheight@px;' alt='@alt@'>
+            <img id='@name@_imgID' class='dpctrl-img' src='@src@'  style='opacity:0.5; max-width:100%; max-height:@maxheight@px;' alt='@alt@'>
         </div>
         <div class='edit_bp_btn-cont' style='width: 100%;position: absolute;bottom: 0;background: transparent; ;'>
                 <div id='edit_buleprintbtn' class='edit_bp@ebsid@' style='height: 20px;width: 70px;display: inline-block;text-align: center;border: 1px solid #ccc;border-radius: 20px;font-size: 12px;padding: 1px;background-color: #eee;cursor: pointer;'>
