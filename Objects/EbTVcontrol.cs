726240b4a37639e1fa38cc87aee0797b79fd80ec
Jith Job <jithjob@expressbase.com.com>
2020-04-20 15:03:05 +0000

    EbTVcontrol class added

---
+++ Objects/EbTVcontrol.cs
@@ -0,0 +1,40 @@
+﻿using ExpressBase.Common;
+using ExpressBase.Common.Extensions;
+using ExpressBase.Common.Objects;
+using ExpressBase.Common.Objects.Attributes;
+using ExpressBase.Common.Structures;
+using ExpressBase.Objects.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Common;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using ExpressBase.Security;
+using System.Globalization;
+using ExpressBase.Common.LocationNSolution;
+using ExpressBase.Common.Constants;
+
+namespace ExpressBase.Objects
+{
+    [EnableInBuilder(BuilderType.WebForm, BuilderType.BotForm)]
+    public class EbTVcontrol : EbControl
+    {
+
+        public EbTVcontrol()
+        {
+
+        }
+
+        [OnDeserialized]
+        public void OnDeserializedMethod(StreamingContext context)
+        {
+            this.BareControlHtml = this.GetBareHtml();
+            this.BareControlHtml4Bot = this.BareControlHtml;
+            this.ObjType = this.GetType().Name.Substring(2, this.GetType().Name.Length - 2);
+            //this.EbDbType = this.EbDbType;
+        }
+    }
+}
