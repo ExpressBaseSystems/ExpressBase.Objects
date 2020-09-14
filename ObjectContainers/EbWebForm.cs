43f1de60d31af54337ac0629ccd27de70bde3f0a
Febin Carlos <febincarlos@expressbase.com>
2020-09-14 17:32:29 +0530

    InputGeoLocation - OSM; EnableExcelImport prop added;

--- ObjectContainers/EbWebForm.cs
+++ ObjectContainers/EbWebForm.cs
@@ -151,6 +151,10 @@ namespace ExpressBase.Objects
         [EnableInBuilder(BuilderType.WebForm)]
         public bool CanSaveAsDraft { get; set; }
 
+        [PropertyGroup(PGConstants.EXTENDED)]
+        [EnableInBuilder(BuilderType.WebForm)]
+        public bool EnableExcelImport { get; set; }
+
         [PropertyGroup(PGConstants.DATA)]
         [EnableInBuilder(BuilderType.WebForm)]
         [PropertyEditor(PropertyEditorType.Collection)]
