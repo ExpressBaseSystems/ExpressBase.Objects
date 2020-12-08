d1480cf3885cc301525102716f02ec5f0653947a
VarghesePaulose <51479206+VarghesePaulose@users.noreply.github.com>
2020-12-08 12:24:31 +0530

    changes for SSE user suspend ,role changes

---
+++ ServiceStack_Artifacts/SecurityMqServices_Artifacts.cs
@@ -0,0 +1,29 @@
+﻿using ExpressBase.Common.EbServiceStack.ReqNRes;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ExpressBase.Objects.ServiceStack_Artifacts
+{
+
+	public class SaveRoleMqRequest : EbMqRequest
+	{
+		
+		[DataMember(Order = 1)]
+		public string SubscriptionId { get; set; }
+
+		[DataMember(Order = 2)]
+		public string ColUserIds { get; set; }
+
+	}
+
+	public class SaveUserMqRequest : EbMqRequest
+	{
+		
+		[DataMember(Order = 1)]
+		public string SubscriptionId { get; set; }		
+
+	}
+
+}
