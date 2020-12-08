7e5c7cb4652cb71e2e063e4951a9243376a8c22e
Amal K Dev <amaldev@expressbase.com>
2020-12-08 18:51:31 +0530

    props

--- Objects/MobilePage/EbMobileEnums.cs
+++ Objects/MobilePage/EbMobileEnums.cs
@@ -67,4 +67,14 @@
         RadarChart,
         RadialGaugeChart,
     }
+
+    public enum MobileTextWrap
+    {
+        NoWrap = 1,
+        HeadTruncation = 2,
+        CharacterWrap = 3,
+        MiddleTruncation = 4,
+        TailTruncation = 5,
+        WordWrap = 6
+    }
 }
