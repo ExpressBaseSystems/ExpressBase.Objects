fa29f0f861679916cf7170208f32daea3c8a8329
VarghesePaulose <51479206+VarghesePaulose@users.noreply.github.com>
2021-01-27 15:37:59 +0530

    phone control-otp,update usermenu(sse)-save user,usergroup

--- ServiceStack_Artifacts/SecurityMqServices_Artifacts.cs
+++ ServiceStack_Artifacts/SecurityMqServices_Artifacts.cs
@@ -9,7 +9,7 @@ namespace ExpressBase.Objects.ServiceStack_Artifacts
 
 	public class SaveRoleMqRequest : EbMqRequest
 	{
-		
+
 		[DataMember(Order = 1)]
 		public string SubscriptionId { get; set; }
 
@@ -29,11 +29,49 @@ namespace ExpressBase.Objects.ServiceStack_Artifacts
 
 	}
 
+	public class SuspendUserMqRequest : EbMqRequest
+	{
+
+		[DataMember(Order = 1)]
+		public string SubscriptionId { get; set; }
+
+	}
 	public class SaveUserMqRequest : EbMqRequest
 	{
-		
 		[DataMember(Order = 1)]
-		public string SubscriptionId { get; set; }		
+		public string SubscriptionId { get; set; }
+
+		[DataMember(Order = 2)]
+		public string LocationAdd { get; set; }
+
+		[DataMember(Order = 3)]
+		public string LocationDelete { get; set; }
+
+		[DataMember(Order = 4)]
+		public List<string> NewRole_Ids { get; set; } = new List<string>();
+
+		[DataMember(Order = 5)]
+		public List<string> OldRole_Ids { get; set; } = new List<string>();
+
+		[DataMember(Order = 6)]
+		public List<string> OldUserGroups { get; set; } = new List<string>();
+
+		[DataMember(Order = 7)]
+		public List<string> NewUserGroups { get; set; } = new List<string>();
+
+	}
+	public class SaveUserGroupMqRequest : EbMqRequest
+	{
+		[DataMember(Order = 1)]
+		public string SubscriptionId { get; set; }
+
+		[DataMember(Order = 2)]
+		public List<string> OldUserGroups { get; set; } = new List<string>();
+
+		[DataMember(Order = 3)]
+		public List<string> NewUserGroups { get; set; } = new List<string>();
+
+
 
 	}
 
