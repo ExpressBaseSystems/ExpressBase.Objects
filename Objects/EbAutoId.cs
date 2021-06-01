811b76835a6df8b2359f235315661b3bfcb3a02d
Febin Carlos <febincarlos@expressbase.com>
2021-06-01 10:51:21 +0400

    AutoId - Sql Script - ExtraSqlParam check added

--- Objects/EbAutoId.cs
+++ Objects/EbAutoId.cs
@@ -189,9 +189,9 @@ namespace ExpressBase.Objects
 
                     if (!string.IsNullOrWhiteSpace(this.Script?.Code))
                     {
+                        EbWebForm WebForm = args.webForm as EbWebForm;
                         if (this.Script.Lang == ScriptingLanguage.CSharp)
                         {
-                            EbWebForm WebForm = args.webForm as EbWebForm;
                             if (WebForm.FormGlobals == null)
                                 WebForm.FormGlobals = GlobalsGenerator.GetCSharpFormGlobals_NEW(WebForm, WebForm.FormData, WebForm.FormDataBackup, args.DataDB, null, false);
 
@@ -206,7 +206,10 @@ namespace ExpressBase.Objects
                             SqlCode = this.Script.Code;
                             List<Param> _params = SqlHelper.GetSqlParams(SqlCode);
                             foreach (Param _p in _params)
-                                SqlCode = SqlCode.Replace(_p.Name, _p.Name + crudContext);
+                            {
+                                if (!EbFormHelper.IsExtraSqlParam(_p.Name, WebForm.TableName))
+                                    SqlCode = SqlCode.Replace(_p.Name, _p.Name + crudContext);
+                            }
                         }
                         else
                             throw new FormException("Unable to process", (int)HttpStatusCode.InternalServerError, $"Invalid script lang {this.Script.Lang} for AutoId: {args.cField.Name}", "EbAutoId => ParameterizeControl");
