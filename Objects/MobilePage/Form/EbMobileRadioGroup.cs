136355c8e32745e3f3f409d7d3ca6a9a27f85009
Febin Carlos <febincarlos@expressbase.com>
2021-12-20 17:04:40 +0400

    mob radio group - options [prop add]

--- Objects/MobilePage/Form/EbMobileRadioGroup.cs
+++ Objects/MobilePage/Form/EbMobileRadioGroup.cs
@@ -2,9 +2,16 @@
 using ExpressBase.Common.Objects;
 using ExpressBase.Common.Objects.Attributes;
 using ExpressBase.Common.Structures;
+using System.Collections.Generic;
 
 namespace ExpressBase.Objects
 {
+    public enum EbMobileRGValueType
+    {
+        Integer = 11,
+        String = 16
+    }
+
     [EnableInBuilder(BuilderType.MobilePage)]
     public class EbMobileRadioGroup : EbMobileControl
     {
@@ -14,7 +21,12 @@ namespace ExpressBase.Objects
 
         [EnableInBuilder(BuilderType.MobilePage)]
         [PropertyGroup("Behavior")]
-        public EbValueType ValueType { get; set; }
+        public EbMobileRGValueType ValueType { get; set; }
+
+        [EnableInBuilder(BuilderType.MobilePage)]
+        [PropertyEditor(PropertyEditorType.Collection)]
+        [PropertyGroup("Data")]
+        public List<EbMobileRGOption> Options { get; set; }
 
         public override string GetDesignHtml()
         {
@@ -28,13 +40,26 @@ namespace ExpressBase.Objects
 
         public override EbControl GetWebFormControl(int counter)
         {
-            return new EbRadioGroup
+            EbRadioGroup ebRadio = new EbRadioGroup
             {
                 EbSid = this.EbControlType + counter,
                 Name = this.Name,
                 Margin = new UISides { Top = 0, Bottom = 0, Left = 0, Right = 0 },
-                Label = this.Label
+                Label = this.Label,
+                Options = new List<EbRadioOptionAbstract>()
             };
+
+            foreach (EbMobileRGOption opt in this.Options)
+            {
+                ebRadio.Options.Add(new EbRadioOption()
+                {
+                    EbSid = opt.EbSid,
+                    Name = opt.Name,
+                    Label = opt.DisplayName,
+                    Value = opt.Value
+                });
+            }
+            return ebRadio;
         }
 
         public override void UpdateWebFormControl(EbControl control)
@@ -44,5 +69,21 @@ namespace ExpressBase.Objects
 
             base.UpdateWebFormControl(control);
         }
+    }
+
+
+    [EnableInBuilder(BuilderType.MobilePage)]
+    public class EbMobileRGOption : EbMobilePageBase
+    {
+        [HideInPropertyGrid]
+        [EnableInBuilder(BuilderType.MobilePage)]
+        public string EbSid { get; set; }
+
+        [EnableInBuilder(BuilderType.MobilePage)]
+        public override string DisplayName { set; get; }
+
+        [EnableInBuilder(BuilderType.MobilePage)]
+        [Alias("Data")]
+        public string Value { set; get; }
     }
 }
