659c5c6e6948698f4109792263dbf90caafe0542
Febin Carlos <febincarlos@expressbase.com>
2022-01-19 20:19:59 +0400

    error bin - data insert

---
+++ ObjectContainers/WebFormRelated/FormDraftsHelper.cs
@@ -0,0 +1,110 @@
+﻿using ExpressBase.Common;
+using ExpressBase.Common.Structures;
+using ExpressBase.Objects.ServiceStack_Artifacts;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Net;
+using System.Text;
+
+namespace ExpressBase.Objects.WebFormRelated
+{
+    public enum FormDraftTypes
+    {
+        NormalDraft,
+        ErrorBin
+    }
+
+    public class FormDraftsHelper
+    {
+        public static InsertDataFromWebformResponse SubmitErrorAndGetResponse(IDatabase DataDB, EbWebForm Form, InsertDataFromWebformRequest request, Exception ex)
+        {
+            try
+            {
+                Console.WriteLine("SaveErrorSubmission start");
+                Dictionary<string, string> MetaData = new Dictionary<string, string>();
+
+                string Qry = $@"
+INSERT INTO eb_form_drafts 
+(
+    title, 
+    form_data_json, 
+    form_ref_id, 
+    message, 
+    stack_trace, 
+    is_submitted, 
+    eb_loc_id, 
+    eb_created_by, 
+    eb_created_at, 
+    eb_del,
+    draft_type,
+    eb_signin_log_id
+)
+VALUES 
+(
+    @title, 
+    @form_data_json, 
+    @form_ref_id, 
+    @error_message, 
+    @error_stacktrace, 
+    'F', 
+    {request.CurrentLoc}, 
+    {request.UserId}, 
+    {DataDB.EB_CURRENT_TIMESTAMP}, 
+    'F',
+    {(int)FormDraftTypes.ErrorBin},
+    {Form.UserObj.SignInLogId}
+); 
+SELECT eb_currval('eb_error_submissions_id_seq');";
+
+                DbParameter[] parameters = new DbParameter[]
+                {
+                    DataDB.GetNewParameter("title", EbDbTypes.String, Form.DisplayName),
+                    DataDB.GetNewParameter("form_data_json", EbDbTypes.String, request.FormData),
+                    DataDB.GetNewParameter("form_ref_id", EbDbTypes.String, Form.RefId),
+                    DataDB.GetNewParameter("error_message", EbDbTypes.String, ex.Message),
+                    DataDB.GetNewParameter("error_stacktrace", EbDbTypes.String, ex.StackTrace)
+                };
+
+                EbDataSet ds = DataDB.DoQueries(Qry, parameters);
+                int _id = Convert.ToInt32(ds.Tables[0].Rows[0][0]);
+                Console.WriteLine("SaveErrorSubmission returning");
+
+                return new InsertDataFromWebformResponse()
+                {
+                    Message = "Error submission saved",
+                    RowId = -1,
+                    RowAffected = 1,
+                    AffectedEntries = "Error submission id: " + _id,
+                    Status = (int)HttpStatusCode.OK,
+                    MetaData = MetaData
+                };
+            }
+            catch (Exception _ex)
+            {
+                Console.WriteLine("Exception in SubmitErrorAndGetResponse\nMessage" + ex.Message + "\nStackTrace" + ex.StackTrace);
+
+                if (ex is FormException formEx)
+                {
+                    return new InsertDataFromWebformResponse()
+                    {
+                        Message = formEx.Message,
+                        Status = formEx.ExceptionCode,
+                        MessageInt = formEx.MessageInternal + ": " + _ex.Message,
+                        StackTraceInt = formEx.StackTraceInternal
+                    };
+                }
+                else
+                {
+                    return new InsertDataFromWebformResponse()
+                    {
+                        Message = FormErrors.E0132 + ex.Message,
+                        Status = (int)HttpStatusCode.InternalServerError,
+                        MessageInt = "Exception in SubmitErrorAndGetResponse[service]: " + _ex.Message,
+                        StackTraceInt = ex.StackTrace
+                    };
+                }
+            }
+        }
+    }
+}
