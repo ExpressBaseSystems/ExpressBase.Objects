e27d22c03ff6324a4d006662582ce1a49d7bc7fc
Febin Carlos <febincarlos@expressbase.com>
2022-12-19 11:27:57 +0530

    MobileSignaturePad ctrl

---
+++ Objects/MobilePage/Form/EbMobileSignaturePad.cs
@@ -0,0 +1,44 @@
+﻿using ExpressBase.Common.Extensions;
+using ExpressBase.Common.Objects;
+using ExpressBase.Common.Objects.Attributes;
+using System.Collections.Generic;
+
+namespace ExpressBase.Objects
+{
+    [EnableInBuilder(BuilderType.MobilePage)]
+    public class EbMobileSignaturePad : EbMobileControl
+    {
+        public override bool DoNotPersist { get; set; }
+        public override bool Unique { get; set; }
+        public override EbScript ValueExpr { get; set; }
+        public override EbScript DefaultValueExpression { get; set; }
+        public override List<EbMobileValidator> Validators { set; get; }
+
+        public override string GetDesignHtml()
+        {
+            return @"<div class='eb_stacklayout mob_control dropped' id='@id' eb-type='EbMobileSignaturePad' tabindex='1' onclick='$(this).focus()'>
+                            <label class='ctrl_label'> @Label </label>
+                            <div class='eb_ctrlhtml'>
+                                <div class='dp-avatar'>
+                                    <img src='/images/image.png'/>
+                                </div>
+                                <div class='dp-btn-container'>
+                                    <button class='eb_mob_fupbtn' style='width:100%'><i class='fa fa-pencil-square-o'></i></button>
+                                </div>
+                            </div>
+                        </div>".RemoveCR().DoubleQuoted();
+        }
+
+        public override EbControl GetWebFormControl(int counter)
+        {
+            return new EbDisplayPicture
+            {
+                EbSid = this.EbControlType + counter,
+                Name = this.Name,
+                Margin = new UISides { Top = 0, Bottom = 0, Left = 0, Right = 0 },
+                Label = this.Label
+            };
+        }
+
+    }
+}
