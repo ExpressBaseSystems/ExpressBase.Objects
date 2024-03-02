89ffd49ae327c87c278207c9ca5342eda9c4ca0b
Febin Carlos <febincarlos@expressbase.com>
2024-03-02 16:27:42 +0530

    BuildInfo class moved to objects

---
+++ Singletons/BuildInfo.cs
@@ -0,0 +1,11 @@
+﻿using ExpressBase.Common.Extensions;
+using System;
+using System.Reflection;
+
+namespace ExpressBase.Objects.Singletons
+{
+    public static class BuildInfo
+    {
+        public static readonly string Md5Version = Assembly.GetAssembly(typeof(BuildInfo)).GetName().Version.ToString().ToMD5Hash();
+    }
+}
